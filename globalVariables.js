f4b108acf284e3dc6244dece50bb8b03d2d3b61a
tahmid17 <timambinsaif462@gmail.com>
2021-05-07 08:42:17 +0600

    completed globalVariables.js module

---
+++ globalVariables.js
@@ -0,0 +1,20 @@
+//__dirname -path to the current directory
+const currentDirectory=__dirname;
+console.log(currentDirectory);
+
+// __filename=filename
+
+const filename=__filename;
+console.log(filename);
+
+//require()= it is a function to use module
+
+const fs=require("fs");
+console.log(fs);
+
+//module = info about current module
+
+console.log(module);
+
+//process =give information abou where the programme is being executed
+console.log(process);

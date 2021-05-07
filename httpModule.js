ca184cc4b63efe71358cff3b83fe257313e2081f
tahmid17 <timambinsaif462@gmail.com>
2021-05-07 12:22:33 +0600

    completed loadContent.js and modified httpModule

--- httpModule.js
+++ httpModule.js
@@ -1,17 +1,33 @@
 const http = require("http");
-
+const allContent = require("./loadContent");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  //console.log(req);
   if (req.url == "/") {
-    res.write("<h1>This is base URL!</h1>");
+    res.write(allContent.index);
     res.end();
-  } else if (req.url == "/home") {
-    res.write("<h1>hello!</h1>");
+  } else if (req.url == "/index") {
+    res.write(allContent.index);
+    res.end();
+  } else if (req.url == "/blog") {
+    res.write(allContent.blog);
+    res.end();
+  } else if (req.url == "/about") {
+    res.write(allContent.about);
+    res.end();
+  } else if (req.url == "/contact") {
+    res.write(allContent.contact);
+    res.end();
+  } else if (req.url == "/pricing") {
+    res.write(allContent.pricing);
+    res.end();
+  } else if (req.url == "/services") {
+    res.write(allContent.services);
+    res.end();
+  } else if (req.url == "/work") {
+    res.write(allContent.work);
     res.end();
   } else {
-    res.write(
-      "<h1>THIS PAGE DOESN'T EXIST!</h1><br><a href='/'>GO TO BASE</a>"
-    );
+    res.write("<h1>Page does not exist.</h1></a>");
     res.end();
   }
 });
